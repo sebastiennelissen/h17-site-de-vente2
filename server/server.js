1c80ce821af75cae45e55380759ecf641dffa9bb
Renaud Vincent <renaud.f.v@gmail.com>
2017-01-22 16:42:24 -0500

    Express and SASS setup, serving static files and autobuilding for stylesheets

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// NPM Modules import
+var express = require('express');
+
+// Modules from node
+var path = require('path')
+
+var app = express();
+
+// Makes every files in public/ visible by the express server
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/', function (req, res) {
+	// Can call directly index.html because of express.static
+	res.sendFile('index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
